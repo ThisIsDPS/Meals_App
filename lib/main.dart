fe332670c5a384b578f96651c757354bc2f28f9f
DamanPreet Singh <daman08@DamanPreets-MacBook-Air.local>
2023-06-27 18:22:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Meals App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Meals App'),
+        ),
+        body: const Center(
+          child: Text('To be continued...'),
+        ),
+      ),
+    ),
+  );
+}
